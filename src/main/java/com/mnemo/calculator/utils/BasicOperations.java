cfaeaa60b5df454c39a8c73ced8e7d05b47703b4
Daniel Hita Jiménez <d.hitajimenez@mnemo.net>
2022-02-04 13:18:56 +0100

    basicOperations

---
+++ src/main/java/com/mnemo/calculator/utils/BasicOperations.java
@@ -0,0 +1,13 @@
+package com.mnemo.calculator.utils;
+
+public abstract class BasicOperations {
+
+	public static double add(Double a, Double b) {
+		return a + b;
+	}
+	
+	public static double subtract(Double a, Double b) {
+		return a - b;
+	}
+	
+}
